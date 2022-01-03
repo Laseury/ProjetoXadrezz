af4a9caa976ac5c361efcff31e3942a15b0edc34
Luiz Fernando <luizfernando7254@gmail.com>
2022-01-02 23:24:54 -0300

    Novas telas adicionada para o projeto

--- application/Xadrez.java
+++ application/Xadrez.java
@@ -5,7 +5,7 @@ import chess.ChessMatch;
 
 public class Xadrez {
     public static void main(String[] args) {
-        //Programa onde o Xadrez funcionar
+        //Programa onde o Xadrez funcionar, Tetse
         ChessMatch chessMatch = new ChessMatch();
 
         UI.printBoard(chessMatch.getPieces());
